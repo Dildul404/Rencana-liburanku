2ec9bb3ac92eaf25af40c02b10654aec238f40f4
Dildul404 <justenjoycode09@gmail.com>
2026-04-10 22:15:21 +0700

    Menambahkan konten yang diperlukan ke beberapa file, dan style nya

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,10 +6,13 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home</title>
     @vite('resources/css/app.css')
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;400;700&display=swap" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1" type="module"></script>
 </head>
 <body>
     <x-Navigation></x-Navigation>
-    {{ $slot }}
+    <section class="p-10">
+        {{ $slot }}
+    </section>
 </body>
 </html>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,18 @@
 <x-layout>
-    Home
+    {{-- Destinasi / section --}}
+    <section>
+        <h1 class="text-center mt-25 mb-10 text-3xl font-bold">Tenang semua sudah direncanakan</h1>
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            <div class="p-10 bg-[#F77F00]">1</div>
+            <div class="p-10 bg-[#F77F00]">2</div>
+            <div class="p-10 bg-[#F77F00]">3</div>
+            <div class="p-10 bg-[#F77F00]">4</div>
+            <div class="p-10 bg-[#F77F00]">5</div>
+            <div class="p-10 bg-[#F77F00]">6</div>
+        </div>
+    </section>
+    {{-- Navigasi rencana liburan --}}
+    <div class="text-center">
+        <a href="/rencana_liburan" class="inline-block text-center p-3 mt-6 bg-black text-white rounded-2xl cursor-pointer transition duration-200 ease-out hover:bg-gray-900 hover:-translate-y-1 hover:shadow-lg active:scale-95">Belum cukup? Buat lagi yuk -></a>
+    </div>
 </x-layout>
